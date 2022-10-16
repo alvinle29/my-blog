3e776c83a9e8fdbff58739f51d75ae0f5dc48dbc
alvinle29 <bach938@gmail.com>
2022-10-17 00:36:24 +0300

    comments components done

--- services/index.js
+++ services/index.js
@@ -132,10 +132,26 @@ export const submitComment = async () => {
   const result = await fetch('/api/comments', {
     method: 'POST',
     headers: {
-      'Content-Type:' :'application/json'
+      'Content-Type:': 'application/json'
     },
     body: JSON.stringify(obj)
   })
 
   return result.json()
+}
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: {post: {slug: $slug}}) {
+        name
+        createdAt
+        comments
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.categories
 }
