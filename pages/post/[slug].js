70ba6f085b656ac30d48e0a2c5fd8ea6366313e1
alvinle29 <bach938@gmail.com>
2022-10-19 02:32:49 +0300

    fix category links for deploy

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -36,7 +36,6 @@ export default PostDetails
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
-  console.log(params)
   const data = await getPostDetails(params.slug)
   return {
     props: {
