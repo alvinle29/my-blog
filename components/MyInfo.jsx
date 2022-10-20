22bd62e5e97d00b83bd3c21f2a0ffe5ed551f74d
alvinle29 <bach938@gmail.com>
2022-10-20 18:03:32 +0300

    change picture position

--- components/MyInfo.jsx
+++ components/MyInfo.jsx
@@ -11,7 +11,7 @@ const MyInfo = () => {
   }, [])
 
   return (
-    <div className="bg-white shadow=lg rounded-lg p-8 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
       <h3 className="text-xl mb-4 font-semibold border-b pb-4">
         tớ là ai ?
       </h3>
