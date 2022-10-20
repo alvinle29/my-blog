bfdf41ee5158ae42024e1e87bb78d1968acff542
alvinle29 <bach938@gmail.com>
2022-10-20 11:46:31 +0300

    fix #4

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
-import { Categories, PostWidget, MyInfo, FeaturedPosts } from '../components';
+import { PostCard, Categories, PostWidget, MyInfo, FeaturedPosts } from '../components';
 import { getPosts } from '../services';
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
 export default function Home({ posts }) {
-  const PostCard = dynamic(() => import("../components/PostCard"), {
+  const PostCardTest = dynamic(() => import("../components/PostCard"), {
     ssr: false,
   });
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>chú pé ngu ngục</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
@@ -21,7 +17,7 @@ export default function Home({ posts }) {
             <PostCard key={index} post={post.node} />
           ))}
 
-          {/*<PostCard key={index} post={post.node} />*/}
+          {/*<PostCardTest key={index} post={post.node} />*/}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <MyInfo />
