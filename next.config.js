a526b678e0a93046c729278283bbc0a41dab8d61
alvinle29 <bach938@gmail.com>
2022-10-20 13:42:16 +0300

    change swcMinify to true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: TransformStreamDefaultController,
 }
 
 module.exports = nextConfig
